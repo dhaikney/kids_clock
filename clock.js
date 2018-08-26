90ac7674414f8c2454aa7dcace3faae5823bf94a
David Haikney <david.haikney@couchbase.com>
2018-08-26 17:01:31 +0100

    DH: night images and audio beeps

--- clock.js
+++ clock.js
@@ -1,3 +1,6 @@
+var nightTime = false;
+
+
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -5,6 +8,11 @@ function startTime() {
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
+    if (s >= 20 || s < 7 ) {
+        nightTime = true;
+    }
+    else
+        {nightTime = false;}
     document.getElementById('clock').innerHTML =
     h + " : " + m + " : " + s;
     var t = setTimeout(startTime, 500);
@@ -14,20 +22,29 @@ function checkTime(i) {
     return i;
 }
 
-var backgrounds = [
+var day_backgrounds = [
   'moana_1.jpg','moana_2.jpg', 'moana_3.jpg', 'moana_4.jpg'];
-
+var night_backgrounds = [
+  'night_1.jpg','night_2.jpg', 'night_3.jpg', 'night_4.jpg'];
 
 var imgIdx = 0;
 function swapBackgrounds() {
+    var backgrounds;
+    if (nightTime) {
+        backgrounds = night_backgrounds;
+    }
+    else {
+       backgrounds = day_backgrounds;
+    }
   if(++imgIdx >= backgrounds.length) {
+    var audio = new Audio('img/alarm_beep.mp3');
+audio.play();
     imgIdx = 0;
   }
-  console.log(backgrounds[imgIdx]);
-  console.log(imgIdx);
+
   $('.background').animate({ opacity: 0}, 500, function() {
     $('.background').css("background-image", "url('img/" + backgrounds[imgIdx] + "')").animate({opacity: 1},500);
   });
 }
 
-setInterval(swapBackgrounds, 30000);
+setInterval(swapBackgrounds, 5000);
