7d753ab7725fac646a98b70e1fc35d0ff9714690
David Haikney <david.haikney@couchbase.com>
2018-08-26 17:41:28 +0100

    DH: corrected inital refresh rate

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 var nightTime = false;
 var audio = new Audio('img/alarm_beep.mp3');
-var interval =  setInterval(swapBackgrounds, 30 * 60 * 1000) ;
+var interval =  setInterval(swapBackgrounds, 5 * 60 * 1000) ;
 
 function startTime() {
     var today = new Date();
