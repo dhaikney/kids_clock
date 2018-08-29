61a54e9dd0b8edee055b900139bcc7477d6b72e9
David Haikney <david.haikney@couchbase.com>
2018-08-29 20:16:04 +0100

    101 dalmation pics for Isla

--- clock.js
+++ clock.js
@@ -1,6 +1,17 @@
 var nightTime = false;
-var audio = new Audio('img/alarm_beep.mp3');
-var interval =  setInterval(swapBackgrounds, 5 * 60 * 1000) ;
+var audio = new Audio('img/wake_up.mp3');
+
+var wakeupHour = 7;
+var wakeupMinute=15;
+var bedTimeHour = 20;
+var nightTimeInterval = 10000;// 10 * 60 * 1000;
+var dayTimeInterval = 5 * 60 * 1000;
+var interval =  setInterval(swapBackgrounds, dayTimeInterval) ;
+
+function isItDayTime(h,m) {
+    return ( (h > wakeupHour && h < bedTimeHour ) ||
+        (h == wakeupHour && m >=wakeupMinute) );
+}
 
 function startTime() {
     var today = new Date();
@@ -9,19 +20,19 @@ function startTime() {
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    if (!nightTime && (h >= 20 || h < 7 )) {
+    if (!nightTime && !isItDayTime(h,m)) {
         nightTime = true;
         clearInterval(interval);
         swapBackgrounds();
-        interval = setInterval(swapBackgrounds, 10 * 60 * 1000);
+        interval = setInterval(swapBackgrounds, nightTimeInterval);
     }
-    else if (nightTime && h >=7 && h <20){
+    else if (nightTime && isItDayTime(h,m)){
         // Time to wakeup! 
         nightTime = false;
         clearInterval(interval);
         swapBackgrounds();
         audio.play();
-        interval = setInterval(swapBackgrounds, 5 * 60 * 1000);
+        interval = setInterval(swapBackgrounds, dayTimeInterval);
     }
     document.getElementById('clock').innerHTML =
     h + " : " + m + " : " + s;
@@ -33,9 +44,12 @@ function checkTime(i) {
 }
 
 var day_backgrounds = [
-  'moana_1.jpg','moana_2.jpg', 'moana_3.jpg', 'moana_4.jpg'];
+    'moana_1.jpg','moana_2.jpg', 'moana_3.jpg', 'moana_4.jpg',
+    'dalmation_1.jpg','dalmation_2.jpg','dalmation_3.jpg','dalmation_4.jpg',
+    'dalmation_5.jpg', 'dalmation_6.jpg', 'dalmation_7.jpg'
+];
 var night_backgrounds = [
-  'night_1.jpg','night_2.jpg', 'night_3.jpg', 'night_4.jpg'];
+  'night_1.jpg','night_2.jpg', 'night_3.jpg', 'night_4.jpg', 'night_5.jpg'];
 
 var imgIdx = 0;
 function swapBackgrounds() {
